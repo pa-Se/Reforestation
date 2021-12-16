ce97de7ed042dae3b19abcac1af422955d15fb4e
Lena Goldmann <goldmannlena93@gmail.com>
2021-12-16 15:27:05 +0100

    tagged flowers and trees, implemented counter logic

---
+++ Reforestation/Assets/Scripts/FlowerCounter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlowerCounter : MonoBehaviour {
+    public Text flowerText;
+
+    // Update is called once per frame
+    void Update() {
+        List<GameObject> flowers = new List<GameObject>(GameObject.FindGameObjectsWithTag("Daffodil"));
+        flowers.AddRange(new List<GameObject>(GameObject.FindGameObjectsWithTag("Rose")));
+
+        int flowerCount = flowers.Count;
+        flowerText.text = flowerCount.ToString();
+    }
+}
